f224409a0fb0717017795d5d3bf1a29c8bf1f727
Robert O'Connor <rob@robbyoconnor.me>
2014-04-17 04:49:45 -0400

    More work towards the finished product...added code to populate the remainder of the PCB

--- project2/v2/src/PCB.scala
+++ project2/v2/src/PCB.scala
@@ -2,24 +2,22 @@ object PID {
   var pid = -1
 }
 class PCB {
-  var memory_start_region: Int = 0
+  var memoryStartRegion: Int = 0
 
   var readwrite: String = ""
 
-  var file_name: String = ""
+  var fileName: String = ""
 
-  var file_size: Int = 0
+  var fileSize: Int = 0
 
   var cylinder: Int = 0
+
+  var tau: Int = 0
 
   var pid: Int = PID.pid + 1
 
   PID.pid += 1
   pid = PID.pid
 
-  def populatePCB() = {
-    memory_start_region = readLine();
-    check
-
-  }
 }
+
--- project2/v2/src/Utils.scala
+++ project2/v2/src/Utils.scala
@@ -18,7 +18,7 @@ object Utils {
 
   def validateInput(input:String): Boolean = {
     input match {
-      case r"^[AtSQq]{1}|^[pPdDcC]\d+|[rdpc]{1}" => true
+      case r"^[AtSQq]{1}|^[pPdDcC]\d+|[rdpc]{1}|[rw]{1}" => true
       case _ => false
     }
   }
@@ -36,12 +36,42 @@ object Utils {
       case _  => false
     }
   }
+
   def extractDeviceInfo(input: String): (String,Int) = {
-    var regex:scala.util.matching.Regex = """([PpDdCc]{1})(\d+)""".r
+    val regex:scala.util.matching.Regex = """([PpDdCc]{1})(\d+)""".r
     input match {
       case regex(device,deviceNo) => (device,Integer.parseInt(deviceNo))
       case _ => ("",-1)
     }
 
+  }
+
+  def  populatePCB(pcb: PCB, printer:Boolean = false) {
+    val fileName:String = readLine("Enter a file name: ")
+    pcb.fileName = fileName
+
+    pcb.readwrite = readLine("Is this a read or a write (r/w): ")
+    while(!validateInput(pcb.readwrite)) {
+      pcb.readwrite = readLine("Invalid input for read/write! You must only enter r or w! Try again: ")
+    }
+    if(pcb.readwrite == "w") {
+      var fileSize =  readLine("How big is the file: ")
+      while(!checkInt(fileSize)) {
+         fileSize = readLine("Only enter integers for file size! Try again: ")
+      }
+      pcb.fileSize = fileSize.toInt
+
+    }
+    var memoryStartRegion = readLine("Enter a memory start region (must be an integer): ")
+    while(!(checkInt(memoryStartRegion) && memoryStartRegion.toInt>0)) {
+      memoryStartRegion = readLine("Must be an integer for memory start region! Try again: ")
+    }
+    pcb.memoryStartRegion = memoryStartRegion.toInt
+
+    var cylinder = readLine("Enter a cylinder #: ")
+    while (!(checkInt(cylinder) && cylinder.toInt >= 0)) {
+      cylinder = readLine("Must be an integer for cylinder! Try again: ")
+    }
+    pcb.cylinder = cylinder.toInt
   }
 }
