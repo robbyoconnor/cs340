7d27303e87187ae0f341ab6ff2535b9df712bd75
Robert O'Connor <rob@robbyoconnor.me>
2014-05-22 06:44:39 -0400

    .

--- project3/JobPool.scala
+++ project3/JobPool.scala
@@ -3,36 +3,37 @@ class JobPool {
   import scala.collection.mutable.ArrayBuffer
 
   val queue = new ArrayBuffer[PCB]
-  
-  def empty:Boolean = queue.isEmpty
-  
+
+  def empty: Boolean = queue.isEmpty
+
   def size: Int = queue.size
-  
+
   def enqueue(pcb: PCB): PCB = {
     queue += pcb
     pcb
   }
 
   def dequeue: PCB = queue.remove(0)
-  
-  def processJobPool(os:OS) {        
-    if(queue.isEmpty) {
+
+  def processJobPool(os: OS) {
+    if (queue.isEmpty) {
       return
-    }
-    for (job <- queue) {      
-      val _pcb = os.allocate(job.limit, job)      
-      if (_pcb.isDefined) {
-        os.memory += job
-        os.readyqueue.enqueue(job)        
-        queue -= job
-        println(s"Process ${job.pid} has been moved from the job pool to the ready queue!")
-      } 
+    } else {
+      for (job <- queue) {
+        val _pcb = os.allocate(job.limit, job)
+        if (_pcb.isDefined) {
+          os.memory += job
+          os.readyqueue.enqueue(job)
+          queue -= job
+          println(s"Process ${job.pid} has been moved from the job pool to the ready queue!")
+        }
+      }
     }
   }
 
-  def snapshot:String ={
+  def snapshot: String = {
     if (queue.isEmpty)
-      "Job Pool is empty"      
+      "Job Pool is empty"
     else {
       var data = new ArrayBuffer[ArrayBuffer[Any]]()
       data += ArrayBuffer("PID", "Limit")
